4b4381883fb748f43ea746f8ef2a72918bebd5e3
shirley89 <yexinxie@163.com>
2023-09-20 16:22:56 +0000

    Update Page.js

--- src/Page.js
+++ src/Page.js
@@ -3,18 +3,19 @@ import SubPage from "./SubPage";
 import SubPage2 from "./SubPage2";
 
 export default function Page({ pageNum, loading, onChange }) {
+  const isParentPage = pageNum === 0;
   return [
-    <h2 key="title">本页内容</h2>,
+    <h2 key="title">{isParentPage ? "" : "子应用"}本页内容</h2>,
     <p key="id">【注意，这是不准确的】同步方式获取的用户ID：{getUserId()}</p>,
-    pageNum === 0 ? (
+    isParentPage ? (
       <SubPage key="subpage" loading={loading} />
     ) : (
       <SubPage2 key="subpage" />
     ),
     <button key="btn" disabled={loading} onClick={onChange}>
-      {pageNum !== 0 && loading ? "正在切换" : "切换账号"}
+      {!isParentPage && loading ? "正在切换" : "切换账号"}
     </button>,
-    pageNum !== 0 ? (
+    !isParentPage ? (
       <p key="tip">【切换完成后会广播同时更新本页数据】</p>
     ) : null,
   ];
