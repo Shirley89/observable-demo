5c595869865ddc16301442eb16bea38dacef91ad
shirley89 <yexinxie@163.com>
2023-09-20 12:03:20 +0000

    Update 2 files

--- src/SubApp2.js
+++ src/SubApp2.js
@@ -14,6 +14,6 @@ export default function SubApp() {
   return [
     <h2 key="title">子应用2内容：</h2>,
     <p key="name">通道获取的用户ID：{user.id}</p>,
-    <iframe key="subapp" title="self" src="https://wrtcwv.csb.app/" />
+    // <iframe key="subapp" title="self" src="https://wrtcwv.csb.app/" />
   ];
 }
