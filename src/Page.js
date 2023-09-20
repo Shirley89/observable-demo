c6120558f3d6232eec02bccb1e8f933c1bdedb61
shirley89 <yexinxie@163.com>
2023-09-20 16:17:53 +0000

    Update Page.js

--- src/Page.js
+++ src/Page.js
@@ -2,17 +2,20 @@ import { getUserId } from "./observables/user";
 import SubPage from "./SubPage";
 import SubPage2 from "./SubPage2";
 
-export default function Page(props) {
+export default function Page({ pageNum, loading, onChange }) {
   return [
     <h2 key="title">本页内容</h2>,
     <p key="id">【注意，这是不准确的】同步方式获取的用户ID：{getUserId()}</p>,
-    props.pageNum === 0 ? (
-      <SubPage key="subpage" loading={props.loading} />
+    pageNum === 0 ? (
+      <SubPage key="subpage" loading={loading} />
     ) : (
       <SubPage2 key="subpage" />
     ),
-    <button key="btn" disabled={props.loading} onClick={props.onChange}>
-      切换账号
+    <button key="btn" disabled={loading} onClick={onChange}>
+      {pageNum !== 0 && loading ? "正在切换" : "切换账号"}
     </button>,
+    pageNum !== 0 ? (
+      <p key="tip">【切换完成后会广播同时更新本页数据】</p>
+    ) : null,
   ];
 }
